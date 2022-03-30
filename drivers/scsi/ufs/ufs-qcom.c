13a22b2f137d112fbf8f7b77dc709dd49aeb278e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-30 18:30:14 +0200

    drivers: scsi: ufs: Import Xiaomi changes
    Change-Id: I6389292472c6fd809ff9a77d4f5bae07d79ae2af

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1332,7 +1332,7 @@ static void ufs_qcom_dev_ref_clk_ctrl(struct ufs_qcom_host *host, bool enable)
 			    UFS_DEVICE_QUIRK_WAIT_AFTER_REF_CLK_UNGATE)
 				usleep_range(50, 60);
 			else
-				udelay(1);
+				udelay(100);
 		}
 
 		host->is_dev_ref_clk_enabled = enable;
