d0ba844d4a9d88b70f7f9a2eeecde56b327c908a
Ivan Vecera <ivan@cera.cz>
2022-09-19 15:42:19 +0200

    input: tp_goodix_driver: Update aod_status
    Field aod_status is currently unused because its value is always
    zero. In stock ROM its value is updated by touchfeature HAL that
    we do not use.
    Set this field to 1 when display goes to low-power (doze) mode
    and to 0 when display resumes.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I68b171819e3ee78a967f01dd9ea658342e5d7a81

--- drivers/input/touchscreen/tp_goodix_driver/goodix_ts_core.c
+++ drivers/input/touchscreen/tp_goodix_driver/goodix_ts_core.c
@@ -2036,11 +2036,13 @@ int goodix_ts_fb_notifier_callback(struct notifier_block *self,
 				return 0;
 			ts_info("suspend by %s", blank == MSM_DRM_BLANK_POWERDOWN ? "blank" :
 			"doze");
+			core_data->aod_status = 1;
 			queue_work(core_data->event_wq, &core_data->suspend_work);
 		} else if (event == MSM_DRM_EVENT_BLANK && blank == MSM_DRM_BLANK_UNBLANK) {
 			if (!atomic_read(&core_data->suspend_stat))
 				return 0;
 			ts_info("resume");
+			core_data->aod_status = 0;
 			queue_work(core_data->event_wq, &core_data->resume_work);
 		}
 	}
