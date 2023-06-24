f8432bd7e5f314d4a19fa279912b004e21eac15b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-06-25 01:26:23 +0200

    fixup! techpack: audio: Import modified Xiaomi changes
    Change-Id: I42d695f0b8df36de301c0ace6cd430e470e87af5

--- techpack/audio/include/dsp/q6adm-v2.h
+++ techpack/audio/include/dsp/q6adm-v2.h
@@ -187,5 +187,4 @@ int adm_programable_channel_mixer(int port_id, int copp_idx, int session_id,
 			int session_type,
 			struct msm_pcm_channel_mixer *ch_mixer,
 			int channel_index);
-
 #endif /* __Q6_ADM_V2_H__ */
